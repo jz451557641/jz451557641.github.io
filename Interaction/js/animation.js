080008789f07dd3dffe47bc9f5e4ca4fae24320b
Jizheng Chen <chenj506@newschool.edu>
2018-12-04 19:14:52 -0500

    q

---
+++ Interaction/js/animation.js
@@ -0,0 +1,27 @@
+var run = document.getElementById('run');
+var isEnter = false;
+document.addEventListener('mousemove', function (e) {
+    if (isEnter) return
+    run.style.display = 'block';
+    run.style.top = e.clientY + 'px';
+    run.style.left = e.clientX - 2 + 'px';
+    });
+var a = document.getElementsByTagName('a');
+for (var i = 0; i < a.length; i++) {
+    a[i].addEventListener('mouseenter', function () {
+    isEnter = true;
+    if(myPage) {
+        run.src = '../img/staticRun.jpg'
+    } else {
+        run.src = 'img/indexRun.jpg'
+    }
+    })
+a[i].addEventListener('mouseleave', function () {
+    isEnter = false;
+    if(myPage) {
+        run.src = '../img/run.gif'
+    } else {
+        run.src = 'img/run.gif'
+    }
+    })
+}
